202d441cf58df1c86feee0ab602a11021e9c4f5a
ctest9104-tech <ctest9104@gmail.com>
2026-06-18 15:04:13 -0300

    Refactor index.js to clean up comments

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Enable CORS for API requests
 app.use(cors());
-
-// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Fallback to index.html for any other requests (Single Page App routing)
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
